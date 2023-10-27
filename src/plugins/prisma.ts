752ef4ee28802d2048673c19069a446c2858bc3c
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2023-10-27 18:05:44 +0800

    chore: camelCase wip

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -1,5 +1,6 @@
-import { PrismaClient } from '@prisma/client'
+import camelCase from 'lodash/camelCase'
 import fp from 'fastify-plugin'
+import { PrismaClient } from '@prisma/client'
 
 // Use TypeScript module augmentation to declare the type of server.prisma to be PrismaClient
 declare module 'fastify' {
@@ -10,6 +11,30 @@ declare module 'fastify' {
 
 export default fp(async (server, options) => {
   const prisma = new PrismaClient()
+
+  type $queryRawType = typeof prisma.$queryRaw
+  prisma.$extends({
+    name: 'camelCase',
+    client: {
+      // @ts-ignore promise
+      async $$queryRaw <T = unknown>(...args: Parameters<$queryRawType>): ReturnType<$queryRawType> {
+        // to camelCase
+        const result = prisma.$queryRaw<T>(...args)
+
+        if (Array.isArray(result) && result.length) {
+          const map = new Map<string, string>()
+          if (typeof result[0] === 'object') {
+            for (const key of result[0]) {
+              map.set(key as string, camelCase(key as string))
+            }
+          }
+        }
+
+        return result
+      },
+    },
+  })
+
 
   await prisma.$connect()
 
